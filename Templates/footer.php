875ecae1d81840c5f30ac4779b85e8bf4434d92c
Sukhmanjit Singh <sukhmanjitsaini04@gmail.com>
2025-09-18 19:55:48 +0200

    PHP_Class2

---
+++ Templates/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+    <hr>
+    <p>@Sukhmanjit Singh 2025</p>
+</div>
+
+
+</body>
+</html>
---
+++ Templates/index.php
@@ -0,0 +1,14 @@
+<?php
+include_once('navbar.php');
+
+?>
+
+<div class="index">
+
+<h1>Estic al index</h1>
+
+</div>
+<?php
+include_once('footer.php');
+
+?>
